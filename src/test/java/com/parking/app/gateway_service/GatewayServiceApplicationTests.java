70bb428986b899b861f6c9f0376648442ebf2685
jorgeql73 <quirogajorge0405@gmail.com>
2025-01-26 17:32:48 -0300

    Agrego gateway para que todas las peticiones sean al mismo puerto 8080

---
+++ src/test/java/com/parking/app/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parking.app.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
